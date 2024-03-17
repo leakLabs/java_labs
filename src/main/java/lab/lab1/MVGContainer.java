82660f2f951df31a70d8c534340ba28cc7364844
Zer-01 <oko48ru@gmail.com>
2024-03-17 23:27:58 +0300

    add constructor, write JavaDoc

--- src/main/java/lab/lab1/MVGContainer.java
+++ src/main/java/lab/lab1/MVGContainer.java
@@ -2,26 +2,61 @@ package lab.lab1;
 
 import java.util.Arrays;
 
+/**
+ * Класс MVGContainer представляет собой контейнер для хранения целых чисел.
+ * Реализован на основе динамического массива, поддерживает операции добавления,
+ * извлечения, удаления элемента по индексу и удаления элемента по значению.
+ */
 public class MVGContainer {
+    private final static int DEFAULT_SIZE = 10;
     private int[] data;
     private int size;
 
-    public MVGContainer() {
-        data = new int[10];
-        size = 0;
+    /**
+     * Конструктор по умолчанию.
+     * Инициализирует контейнер заданным размером массива.
+     * @param contSize Размер контейнера
+     */
+    public MVGContainer(int contSize) {
+        if (contSize < 1) {
+            throw new IllegalArgumentException("Заданный размер меньше 0");
+        }
+        data = new int[contSize];
+        this.size = 0;
     }
 
+    /**
+     * Конструктор по умолчанию.
+     * Инициализирует контейнер начальным размером массива.
+     */
+    public MVGContainer() {
+        this(DEFAULT_SIZE);
+    }
+
+    /**
+     * Добавляет элемент в контейнер.
+     * @param value Значение для добавления в контейнер.
+     */
     public void add(int value) {
         ensureCapacity();
         data[size++] = value;
     }
 
+    /**
+     * Проверка места в массиве, увеличение его размера при необходимости.
+     */
     private void ensureCapacity() {
         if (size == data.length) {
             data = Arrays.copyOf(data, size * 2);
         }
     }
 
+    /**
+     * Возвращает элемент по указанному индексу.
+     * @param index Индекс элемента для возврата.
+     * @return Значение элемента на указанной позиции.
+     * @throws IndexOutOfBoundsException если индекс находится вне границ диапазона [0, size).
+     */
     public int get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException(String.format("Index: %d, Size: %d", index, size));
@@ -29,6 +64,11 @@ public class MVGContainer {
         return data[index];
     }
 
+    /**
+     * Удаляет элемент по указанному индексу.
+     * @param index Индекс удаляемого элемента.
+     * @throws IndexOutOfBoundsException если индекс находится вне границ диапазона [0, size).
+     */
     public void removeAt(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException(String.format("Index: %d, Size: %d", index, size));
@@ -40,6 +80,11 @@ public class MVGContainer {
         size--;
     }
 
+    /**
+     * Удаляет первое вхождение указанного значения в контейнере.
+     * @param value Значение для удаления.
+     * @return true если элемент был найден и удален, иначе false.
+     */
     public boolean remove(int value) {
         for (int i = 0; i < size; i++) {
             if (data[i] == value) {
@@ -50,10 +95,18 @@ public class MVGContainer {
         return false;
     }
 
+    /**
+     * Возвращает количество элементов в контейнере.
+     * @return Количество элементов.
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Возвращает строковое представление контейнера.
+     * @return Строка, содержащая все элементы контейнера, разделенные запятой и пробелом.
+     */
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
