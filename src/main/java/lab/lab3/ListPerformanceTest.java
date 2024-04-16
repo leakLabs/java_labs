a04fca70553ce4dd590169b36711e9e0dbe76187
Zer-01 <oko48ru@gmail.com>
2024-04-16 12:28:03 +0300

    Add javadoc

--- src/main/java/lab/lab3/ListPerformanceTest.java
+++ src/main/java/lab/lab3/ListPerformanceTest.java
@@ -4,48 +4,72 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * Класс для тестирования производительности операций в ArrayList и LinkedList.
+ */
 public class ListPerformanceTest {
     private final static int OPERATIONS_COUNT = 100000;
 
+    /**
+     * Основной метод для запуска тестов производительности.
+     */
     public static void main(String[] args) {
         List<Integer> arrayList = new ArrayList<>();
-        long arrayListAddTime = testAdd(arrayList);
-        long arrayListGetTime = testGet(arrayList);
-        long arrayListRemoveTime = testRemove(arrayList);
+        double arrayListAddTime = testAdd(arrayList);
+        double arrayListGetTime = testGet(arrayList);
+        double arrayListRemoveTime = testRemove(arrayList);
 
         List<Integer> linkedList = new LinkedList<>();
-        long linkedListAddTime = testAdd(linkedList);
-        long linkedListGetTime = testGet(linkedList);
-        long linkedListRemoveTime = testRemove(linkedList);
+        double linkedListAddTime = testAdd(linkedList);
+        double linkedListGetTime = testGet(linkedList);
+        double linkedListRemoveTime = testRemove(linkedList);
 
         System.out.println("Results for " + OPERATIONS_COUNT + " operations:");
         System.out.println("Method       ArrayList   LinkedList");
-        System.out.printf("Add %16dms %10dms\n", arrayListAddTime, linkedListAddTime);
-        System.out.printf("Get %16dms %10dms\n", arrayListGetTime, linkedListGetTime);
-        System.out.printf("Remove %13dms %10dms\n", arrayListRemoveTime, linkedListRemoveTime);
+        System.out.printf("Add %16.2fms %10.2fms\n", arrayListAddTime, linkedListAddTime);
+        System.out.printf("Get %16.2fms %10.2fms\n", arrayListGetTime, linkedListGetTime);
+        System.out.printf("Remove %13.2fms %10.2fms\n", arrayListRemoveTime, linkedListRemoveTime);
     }
 
-    private static long testAdd(List<Integer> list) {
+    /**
+     * Тестирует время добавления элементов в список.
+     *
+     * @param list Список для добавления элементов
+     * @return время в миллисекундах, затраченное на операции
+     */
+    private static double testAdd(List<Integer> list) {
         long startTime = System.nanoTime();
         for (int i = 0; i < ListPerformanceTest.OPERATIONS_COUNT; i++) {
             list.add(i);
         }
-        return (System.nanoTime() - startTime) / 1_000_000;
+        return (System.nanoTime() - startTime) / 1_000_000d;
     }
 
-    private static long testGet(List<Integer> list) {
+    /**
+     * Тестирует время доступа к элементам списка.
+     *
+     * @param list Список, из которого происходит чтение
+     * @return время в миллисекундах, затраченное на операции
+     */
+    private static double testGet(List<Integer> list) {
         long startTime = System.nanoTime();
         for (int i = 0; i < ListPerformanceTest.OPERATIONS_COUNT; i++) {
             list.get(i % list.size());
         }
-        return (System.nanoTime() - startTime) / 1_000_000;
+        return (System.nanoTime() - startTime) / 1_000_000d;
     }
 
-    private static long testRemove(List<Integer> list) {
+    /**
+     * Тестирует время удаления элементов из списка.
+     *
+     * @param list Список, из которого удаляются элементы
+     * @return время в миллисекундах, затраченное на операции
+     */
+    private static double testRemove(List<Integer> list) {
         long startTime = System.nanoTime();
         for (int i = ListPerformanceTest.OPERATIONS_COUNT - 1; i >= 0; i--) {
             list.remove(i % list.size());
         }
-        return (System.nanoTime() - startTime) / 1_000_000;
+        return (System.nanoTime() - startTime) / 1_000_000d;
     }
 }
