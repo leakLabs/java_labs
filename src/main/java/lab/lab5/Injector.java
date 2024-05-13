08b1b17b6b8bbfc1a1ff32b35dbd4b49a0cdd3cb
Zer-01 <oko48ru@gmail.com>
2024-05-13 23:37:33 +0300

    add javadoc

--- src/main/java/lab/lab5/Injector.java
+++ src/main/java/lab/lab5/Injector.java
@@ -13,6 +13,13 @@ import java.util.Properties;
 public class Injector {
     private final Properties properties;
 
+    /**
+     * Конструктор создаёт экземпляр {@code Injector}, загружая свойства из указанного файла.
+     *
+     * @param propFile путь к файлу свойств.
+     * @throws IOException          если происходит ошибка ввода/вывода при работе с файлом.
+     * @throws NullPointerException если {@code propFile} равен {@code null}.
+     */
     public Injector(Path propFile) throws IOException {
         Objects.requireNonNull(propFile);
         properties = new Properties();
@@ -21,10 +28,25 @@ public class Injector {
         }
     }
 
+    /**
+     * Конструктор создаёт экземпляр {@code Injector}, загружая свойства из файла ресурсов.
+     *
+     * @param resourceFileName имя файла ресурсов в classpath.
+     * @throws URISyntaxException если синтаксис URI некорректен.
+     * @throws IOException        если происходит ошибка ввода/вывода при работе с файлом.
+     */
     public Injector(String resourceFileName) throws URISyntaxException, IOException {
         this(Paths.get(ClassLoader.getSystemResource(resourceFileName).toURI()));
     }
 
+    /**
+     * Метод производит инъекцию зависимостей в переданный объект.
+     * Поля объекта, аннотированные как {@link AutoInjectable}, будут инициализированы
+     * объектами классов, указанных в свойствах.
+     *
+     * @param object объект, в который необходимо внедрить зависимости.
+     * @throws NullPointerException если {@code object} равен {@code null}.
+     */
     public void inject(Object object) {
         Objects.requireNonNull(object);
         Class<?> clazz = object.getClass();
@@ -37,6 +59,13 @@ public class Injector {
         }
     }
 
+    /**
+     * Метод пытается внедрить зависимость в поле объекта.
+     *
+     * @param target объект, поле которого должно быть инициализировано.
+     * @param field  поле объекта, в которое должна быть выполнена инъекция.
+     * @throws RuntimeException если происходит ошибка при создании экземпляра класса зависимости.
+     */
     private void tryInjectField(Object target, Field field) {
         String injectingClassName = properties.getProperty(field.getType().getName());
         if (injectingClassName != null) {
